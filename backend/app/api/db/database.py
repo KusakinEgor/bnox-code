73b6ce8aa054dbc58872927dbd7d51b870805c17
Egor <bananaxvii812@gmail.com>
2025-08-11 18:39:15 +0500

    Add engine with sessions for connection Postgresql database

---
+++ backend/app/api/db/database.py
@@ -0,0 +1,27 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.api.config import DATABASE_URL
+
+engine = create_async_engine(
+    DATABASE_URL,
+    echo=True,
+    pool_size=20,
+    max_overflow=0,
+    pool_timeout=30,
+    pool_recucle=1800
+)
+
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autocommit=False,
+    autoflush=False,
+)
+
+
+Base = declarative_base()
+
+async def get_db():
+    async with AsyncSessionLocal() as session:
+        yield session
